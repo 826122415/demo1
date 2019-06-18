338ded39106f5d59432fdf6f2b4ad58cafa57939
826122415 <34120463+826122415@users.noreply.github.com>
2019-06-18 09:54:07 +0800

    测试
    测试文件

---
+++ Main.java
@@ -0,0 +1,34 @@
+package cn.sange.day1;
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+//        Class cls = Student.class;
+        String name = "cn.sange.day1.Docter";
+        Class cls;
+        if (isClassPresent(name)){
+            cls = Class.forName(name);
+        }else{
+            cls = Class.forName("cn.sange.day1.Student");
+        }
+
+        System.out.println("class name:" + cls.getName());
+        System.out.println("class simple name:" + cls.getSimpleName());
+        System.out.println("package name:" + cls.getPackage().getName());
+        System.out.println("is interface:" + cls.isInterface());
+            //Student student = (Student) cls.newInstance();
+            //student.show();
+             Hello hello = (Hello) cls.newInstance();
+             hello.show();
+
+    }
+
+    public static boolean isClassPresent(String name){
+        try {
+            Class.forName(name);
+            return true;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
